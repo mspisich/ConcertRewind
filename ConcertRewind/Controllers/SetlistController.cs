8517bfcf6f56cfdaff4ba4d793148ee7acbd0285
mspisich <mike.spisich@gmail.com>
2017-05-16 16:57:43 -0400

    Add APIKeys.cs model again. Forgot to save last time...

--- ConcertRewind/Controllers/SetlistController.cs
+++ ConcertRewind/Controllers/SetlistController.cs
@@ -106,25 +106,25 @@ namespace ConcertRewind.Controllers
         {
             string apiKey = APIKeys.setlistApiKey;
             
-                HttpWebRequest request =
+            HttpWebRequest request =
 
-                //Load setlist json for chosen artist from setlist.fm API
-                WebRequest.CreateHttp("http://api.setlist.fm/rest/0.1/search/setlists.json?artistName=" + artistName + "&?key=" + apiKey);
+            //Load setlist json for chosen artist from setlist.fm API
+            WebRequest.CreateHttp("http://api.setlist.fm/rest/0.1/search/setlists.json?artistName=" + artistName + "&?key=" + apiKey);
 
-                //Tells the user what browsers we're using
-                request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            //Tells the user what browsers we're using
+            request.UserAgent = @"User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
 
-                //actually grabs the request
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //actually grabs the request
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-                //gets a stream of text
-                StreamReader rd = new StreamReader(response.GetResponseStream());
+            //gets a stream of text
+            StreamReader rd = new StreamReader(response.GetResponseStream());
 
-                //reads to the end of file
-                string ApiText = rd.ReadToEnd();
+            //reads to the end of file
+            string ApiText = rd.ReadToEnd();
 
-                //Converts that text into JSON
-                JObject setlistApi = JObject.Parse(ApiText);
+            //Converts that text into JSON
+            JObject setlistApi = JObject.Parse(ApiText);
 
 
 
